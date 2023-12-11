60a6afa9bad30a83adabe7a47a0044b7402064f4
lifeofsanteri <santeri.savo1@gmail.com>
2023-12-11 16:43:51 +0200

    cors issues

--- pqserver.js
+++ pqserver.js
@@ -6,17 +6,22 @@ const saltRounds = 10;
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+app.use(function (req, res, next) {
+    const allowedOrigins = ['https://home-app-front.onrender.com']; // Add your website URL
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+      res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, UPDATE');
+    next();
+  });
+
 app.use(cors());
 app.use(express.json());
 
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://home-app-front.onrender.com");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-  });
 
 
 const db = new Client({
